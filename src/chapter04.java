671b8b5d44d615c932fca56627baca1add3620cb
kuwahara <kuwahara@localhost>
2021-04-09 12:41:59 +0900

    chapter4 finish

---
+++ src/chapter04.java
@@ -0,0 +1,11 @@
+
+public class chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "seiya";
+		System.out.println(str);
+        str = "lucky";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+}
+}
